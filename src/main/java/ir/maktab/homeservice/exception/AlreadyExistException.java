aa62a143f481ca71588365c8b5a7a1ff49544633
amir mahdi <amirmahdi.seydi@yahoo.com>
2022-05-08 22:22:22 +0430

    Add custom exception

---
+++ src/main/java/ir/maktab/homeservice/exception/AlreadyExistException.java
@@ -0,0 +1,12 @@
+package ir.maktab.homeservice.exception;
+/*
+ * created by Amir mahdi seydi 5/7/2022 10:54 AM
+ */
+
+public class AlreadyExistException extends RuntimeException {
+
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+
+}
