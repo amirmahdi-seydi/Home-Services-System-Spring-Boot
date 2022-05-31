8afe93a302de61123ebce486bd74eccd67b728cb
amir mahdi <amirmahdi.seydi@yahoo.com>
2022-06-01 01:27:13 +0430

    Add mvc config

--- src/main/java/ir/maktab/homeservice/HomeServiceApplication.java
+++ src/main/java/ir/maktab/homeservice/HomeServiceApplication.java
@@ -4,14 +4,26 @@ import ir.maktab.homeservice.service.CustomerService;
 import ir.maktab.homeservice.service.impl.CustomerServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HomeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeServiceApplication.class, args);
-
-
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:3000","http://localhost:8080")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
