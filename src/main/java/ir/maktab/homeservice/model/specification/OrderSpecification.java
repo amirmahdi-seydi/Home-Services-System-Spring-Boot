0d82ce20cf9ae14c7e83e8f74c700911d08fce57
amir mahdi <amirmahdi.seydi@yahoo.com>
2022-06-08 01:17:15 +0430

    Add OrderSpecification.java class for searching by parameter dynamically in order entity

---
+++ src/main/java/ir/maktab/homeservice/model/specification/OrderSpecification.java
@@ -0,0 +1,53 @@
+package ir.maktab.homeservice.model.specification;
+/*
+ * created by Amir mahdi seydi 6/3/2022 10:25 AM
+ */
+
+import ir.maktab.homeservice.model.Order;
+import ir.maktab.homeservice.util.SpecSearchCriteria;
+import org.springframework.data.jpa.domain.Specification;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+
+public class OrderSpecification implements Specification<Order> {
+
+    private SpecSearchCriteria criteria;
+
+    public OrderSpecification(SpecSearchCriteria criteria) {
+        super();
+        this.criteria = criteria;
+    }
+
+    public SpecSearchCriteria getCriteria() {
+        return criteria;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<Order> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        switch (criteria.getOperation()) {
+            case EQUALITY:
+                return builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            case NEGATION:
+                return builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
+            case GREATER_THAN:
+                return builder.greaterThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LESS_THAN:
+                return builder.lessThan(root.get(criteria.getKey()), criteria.getValue().toString());
+            case LIKE:
+                return builder.like(root.get(criteria.getKey()), criteria.getValue().toString());
+            case STARTS_WITH:
+                return builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+            case ENDS_WITH:
+                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
+            case CONTAINS:
+                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            default:
+                return null;
+        }
+    }
+
+
+}
